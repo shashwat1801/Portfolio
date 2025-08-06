30afac8a6b50679c8e3013524efc29974720918a
Shashwat <108486413+shashwat1801@users.noreply.github.com>
2025-08-07 00:27:13 +0530

    Update Navbar.tsx
    Typo

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
 
                 <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
                     <div className='flex items-center justify-between w-full h-auto bordor border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
-                    <a href='#about-me' className='cursor-pointer'>About me</a>
+                    <a href='#about-me' className='cursor-pointer'>About</a>
                     <a href='#skills' className='cursor-pointer'>skills</a>
                     <a href='#projects' className='cursor-pointer'>Projects</a>
                     </div>
